c0da60a22d217df1882a5f21af6ef69b40e3a8ce
shagat <shagat.bing@gmail.com>
2022-05-20 14:27:15 -0400

    css and html for the portfolio website added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FirstSectionComponent } from './components/first-section/first-section.component';
+import { SecondSectionComponent } from './components/second-section/second-section.component';
+import { ThirdSectionComponent } from './components/third-section/third-section.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FirstSectionComponent,
+    SecondSectionComponent,
+    ThirdSectionComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
