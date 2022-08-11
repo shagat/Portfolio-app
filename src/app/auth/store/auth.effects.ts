bfb0200b4e45f8919319a0af26e4ef7450127c02
shagat <shagat.bing@gmail.com>
2022-08-11 19:20:32 +0530

    added handle error and interface of the response data in the effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,9 +1,37 @@
 import { createEffect, Actions, ofType } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
 import * as AuthActions from './auth.actions';
-import { tap } from 'rxjs';
+import { tap, of } from 'rxjs';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+const handleError = (errorRes: any) => {
+  let errorMsg = 'Unexpected error occured!';
+  if (!errorRes.error || !errorRes.error.error) {
+    return of(AuthActions.authenticateFail({ errorMessage: errorMsg }));
+  }
+  switch (errorRes.error.error.message) {
+    case 'EMAIL_EXISTS':
+      errorMsg = 'This email is already in use.';
+      break;
+    case 'EMAIL_NOT_FOUND':
+      errorMsg = 'This email is not valid.';
+      break;
+    case 'INVALID_PASSWORD':
+      errorMsg = 'The password is incorrect.';
+      break;
+  }
+  return of(AuthActions.authenticateFail({ errorMessage: errorMsg }));
+};
 
 @Injectable()
 export class AuthEffects {
@@ -12,6 +40,7 @@ export class AuthEffects {
     private authService: AuthService,
     private router: Router
   ) {}
+
   authLogout$ = createEffect(
     () =>
       this.actions$.pipe(
